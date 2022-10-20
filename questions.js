1c1ca971d91e881d758ca58a7ef496eddfa45b85
ebrljak <78378657+ebrljak@users.noreply.github.com>
2022-10-20 17:41:09 +0200

    questions.js

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+    ]
+    },
+    {
+    numb: 2,
+    question: "How many types of heading does an HTML contain?",
+    answer: "6",
+    options: [
+        "2",
+        "4",
+        "6",
+        "8"
+    ]
+    },
+    {
+    numb: 3,
+    question: "How to insert an image in HTML?",
+    answer: "With img tag",
+    options: [
+        "With ol tag",
+        "With img tag",
+        "With image tag",
+        "With background tag"
+    ]
+    },
+    {
+    numb: 4,
+    question: "In how many ways you can display HTML elements?",
+    answer: "7",
+    options: [
+        "10",
+        "5",
+        "12",
+        "7"
+    ]
+    },
+    {
+    numb: 5,
+    question: "In how many ways can we specify the CSS styles for the HTML element?",
+    answer: "3",
+    options: [
+        "1",
+        "5",
+        "6",
+        "3"
+    ]
+    },
+];
